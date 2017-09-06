0fe56e3e4315c057178cb0063adb0e1000135a07
deepearthgo <31591563+deepearthgo@users.noreply.github.com>
2017-09-06 10:42:33 +0800

    Update centerloss-inl.h

--- centerloss-inl.h
+++ centerloss-inl.h
@@ -4,8 +4,8 @@
  * \brief centerloss 
  * \author deepearthgo
  */
-#ifndef MXNET_OPERATOR_LSOFTMAX_INL_H_
-#define MXNET_OPERATOR_LSOFTMAX_INL_H_
+#ifndef MXNET_OPERATOR_CENTERLOSS_INL_H_
+#define MXNET_OPERATOR_CENTERLOSS_INL_H_
 
 #include <dmlc/logging.h>
 #include <dmlc/parameter.h>
